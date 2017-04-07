1d4d0fa0eaac19d993c3cb8a3706973450d7ae35
rizalio <admin@rizalfakhri.com>
2017-04-07 10:09:50 +0700

    initial commit

---
+++ phpsysinfo-centos.sh
@@ -0,0 +1,50 @@
+!#/bin/sh
+
+#################################################
+#  Server Configuration for Centos 6.8 Final    #
+#              Don't use port 7071              #     
+#################################################
+
+## updating packages
+sudo yum update -y
+
+## installing dependencies
+sudo yum install unzip -y
+sudo yum install nano -y
+sudo yum install sed -y
+sudo yum install screen -y
+
+## installing HTTP Server (Powered By Apache)
+sudo yum install httpd mod_ssl -y
+
+## starting HTTP Services
+sudo /usr/sbin/apachectl start
+
+## fix for hostname resolving
+echo "ServerName	localhost" >> /etc/httpd/conf/httpd.conf
+
+## make automatic start after reboot
+sudo /sbin/chkconfig httpd on
+
+## make sure automatic start is running
+sudo /sbin/chkconfig --list httpd | grep httpd
+
+## install PHP
+sudo yum install php php-mysql php-devel php-gd php-pecl-memcache php-pspell php-snmp php-xmlrpc php-xml -y
+
+## change to port 7071
+sed -i -e 's/Listen 80/Listen 7071/g' /etc/httpd/conf/httpd.conf
+
+##restarting HTTP Services
+sudo /usr/sbin/apachectl restart
+
+## downloading phpsysinfo
+cd /var/www/html
+rm -R *
+wget https://github.com/phpsysinfo/phpsysinfo/archive/master.zip
+unzip master.zip
+cp -r phpsysinfo-master/* /var/www/html
+mv phpsysinfo.ini.new phpsysinfo.ini
+
+## finishing installation
+sudo /usr/sbin/apachectl restart
---
+++ phpsysinfo-ubuntu.sh
@@ -0,0 +1,47 @@
+!#/bin/bash
+
+#################################################
+#  Server Configuration for Ubuntu 16.04 LTS    #
+#              Don't use port 7071              #     
+#################################################
+
+## updating system
+sudo apt update -y
+
+## installing dependencies
+sudo apt install unzip software-properties-common python3-software-properties python-software-properties sed -y
+
+## intalling HTTP Server (Powered by Apache2)
+sudo apt install apache2 -y
+
+## starting the http server
+sudo service apache2 start
+
+## fix for hostname resolving
+echo "ServerName	localhost" >> /etc/apache2/apache2.conf
+
+## restart the http service
+sudo service apache2 restart
+
+## adding the PHP Repository
+sudo add-apt-repository ppa:ondrej/php -y
+
+## update
+sudo apt update
+
+## install php
+sudo apt install php7.0 php7.0-mcrypt php7.0-mysql php7.0-curl php7.0-mbstring libapache2-mod-php7.0 php7.0-common php7.0-xml -y
+
+## change to port 7071
+sed -i -e 's/Listen 80/Listen 7071/g' /etc/apache2/apache2.conf
+
+## install phpsysinfo
+cd /var/www/html
+rm -R *
+wget https://github.com/phpsysinfo/phpsysinfo/archive/master.zip
+unzip master.zip
+cp -r phpsysinfo-master/* /var/www/html
+mv phpsysinfo.ini.new phpsysinfo.ini
+
+## restart
+sudo service apache2 restart 
