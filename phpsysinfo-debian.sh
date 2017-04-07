4889797e31d8f851c9967c1c06f3e0d5e030d759
rizalio <admin@rizalfakhri.com>
2017-04-07 10:45:40 +0700

    initial commit

--- phpsysinfo-debian.sh
+++ phpsysinfo-debian.sh
@@ -15,7 +15,7 @@ sudo apt-get install sed -y
 sudo apt-get install screen -y
 
 ## installing HTTP Server (Powered By Apache)
-sudo yum install apache2 -y
+sudo apt-get install apache2 -y
 
 ## starting HTTP Services
 sudo service apache2 start
@@ -23,11 +23,11 @@ sudo service apache2 start
 ## fix for hostname resolving
 echo "ServerName	localhost" >> /etc/apache2/apache2.conf
 
-## install PHP
-sudo apt-get install php php-mysql php-devel php-gd php-pecl-memcache php-pspell php-snmp php-xmlrpc php-xml -y
+## install PHP5
+sudo apt-get install php5 php5-mysql php-devel php5-gd php5-pecl-memcache php5-pspell php5-snmp php-xml-rpc php-xml libapache2-mod-php5 -y
 
 ## change to port 7071
-sed -i -e 's/Listen 80/Listen 7071/g' /etc/httpd/conf/httpd.conf
+echo "Listen	7071" >> /etc/apache2/apache2.conf
 
 ##restarting HTTP Services
 sudo service apache2 restart
@@ -37,7 +37,7 @@ cd /var/www/html
 rm -R *
 wget https://github.com/phpsysinfo/phpsysinfo/archive/master.zip
 unzip master.zip
-cp -r phpsysinfo-master/* /var/www/html
+cp -r phpsysinfo-master/* /var/www
 mv phpsysinfo.ini.new phpsysinfo.ini
 
 ## finishing installation
