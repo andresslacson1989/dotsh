af9ea2e1091c77066459cdabb3f69855d1ac5030
Rizal Fakhri <herrysantoso0071@gmail.com>
2017-12-28 02:13:46 +0700

    Update phpsysinfo-ubuntu.sh

--- phpsysinfo-ubuntu.sh
+++ phpsysinfo-ubuntu.sh
@@ -33,7 +33,7 @@ sudo apt update
 sudo apt install php7.0 php7.0-mcrypt php7.0-mysql php7.0-curl php7.0-mbstring libapache2-mod-php7.0 php7.0-common php7.0-xml -y
 
 ## change to port 7071
-echo "Listen 7071" >> /etc/apache2/apache2.conf
+echo "Listen 8080" >> /etc/apache2/apache2.conf
 
 ## install phpsysinfo
 cd /var/www/html
